5259a47fb722de8f7f6327651443b9ef714d8f16
Jason Jho <jason@blueapron.com>
2016-04-20 06:44:00 -0400

    Update settings to build 2.1.0-alpha1 release

--- settings.sh
+++ settings.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-: ${SCALA_VERSIONS:="2.10.5 2.11.7"}
+: ${SCALA_VERSIONS:="2.11.7"}
 : ${DEFAULT_SCALA_VERSION:="2.11.7"}
-: ${CONFLUENT_PLATFORM_VERSION:="2.0.1"}
+: ${CONFLUENT_PLATFORM_VERSION:="2.1.0-alpha1"}
 : ${DOCKER_BUILD_OPTS:="--rm=true "}
 : ${DOCKER_TAG_OPTS:="-f "}
-: ${PACKAGE_URL:="http://packages.confluent.io/archive/2.0"}
+: ${PACKAGE_URL:="http://packages.confluent.io/archive/2.1"}
 
 #PRIVATE_REPOSITORY=""
 #PUSH_TO_DOCKER_HUB=
